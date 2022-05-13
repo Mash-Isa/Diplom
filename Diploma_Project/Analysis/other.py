675252183522a225bcd3b39495938fa82c46c027
Maria <isaeva.maria.200@gmail.com>
2022-05-13 21:59:53 +0300

    Add OOP

--- Diploma_Project/Analysis/other.py
+++ Diploma_Project/Analysis/other.py
@@ -8,114 +8,115 @@ from IPython.core.display_functions import display
 from ipywidgets import widgets
 
 
-def choose_file(folder):
-    file_name_widget = widgets.Dropdown(options=os.listdir(folder),
-                                        value=None,
-                                        description='File Name')
-    ui = widgets.HBox([file_name_widget])
+class DataLoader:
 
-    def print_file(file_name):
-        display(file_name)
+    def choose_file(self, folder):
+        file_name_widget = widgets.Dropdown(options=os.listdir(folder),
+                                            value=None,
+                                            description='File Name')
+        ui = widgets.HBox([file_name_widget])
 
-    out = widgets.interactive_output(print_file, {'file_name': file_name_widget})
-    display(ui)
-    return out
+        def print_file(file_name):
+            display(file_name)
 
+        out = widgets.interactive_output(print_file, {'file_name': file_name_widget})
+        display(ui)
+        return out
 
-def read_data_from_csv(folder, selected_file):
-    file = selected_file.outputs[0]['data']['text/plain'][1:-1]
-    path = f'{folder}/{file}'
-    df = pd.read_csv(path)
-    df.sort_index(axis=1, inplace=True)
-    return lower_remove_spaces(df)
+    def read_data_from_csv(self, folder, selected_file):
+        file = selected_file.outputs[0]['data']['text/plain'][1:-1]
+        path = f'{folder}/{file}'
+        df = pd.read_csv(path)
+        df.sort_index(axis=1, inplace=True)
+        return self.lower_remove_spaces(df)
 
+    def read_data_from_csv_for_model(self, folder, selected_file):
+        file = selected_file.outputs[0]['data']['text/plain'][1:-1]
+        path = f'{folder}/{file}'
+        df = pd.read_csv(path)
+        return df
 
-def read_data_from_csv_for_model(folder, selected_file):
-    file = selected_file.outputs[0]['data']['text/plain'][1:-1]
-    path = f'{folder}/{file}'
-    df = pd.read_csv(path)
-    return df
+    def lower_remove_spaces(self, df):
+        categorical_columns = list(df.select_dtypes(['object']).columns)
+        df[categorical_columns] = df[categorical_columns].astype(str).applymap(str.capitalize).applymap(
+            str.strip).replace('Nan', np.nan)
+        return df
 
+    def format_columns(self, df, features_to_encode, features_to_drop, have_label=False, label=None, label_name=None):
+        df[features_to_encode] = df[features_to_encode].astype(str).applymap(str.lower).applymap(str.strip)
+        for i in features_to_encode:
+            df = pd.concat([df, pd.get_dummies(df[i], prefix=i)], axis=1)
+        df = df.loc[:, ~df.columns.str.endswith('nan')]
+        all_features = list(df.select_dtypes(['float', 'uint8']).columns)
+        features = list(set(all_features) - set(features_to_drop))
+        X = df[features]
+        X.replace([np.inf, -np.inf, np.nan], 0, inplace=True)
+        if have_label:
+            y = (df[label] == label_name) * 1
+            return X, y
+        return X
 
-def lower_remove_spaces(df):
-    categorical_columns = list(df.select_dtypes(['object']).columns)
-    df[categorical_columns] = df[categorical_columns].astype(str).applymap(str.capitalize).applymap(str.strip).replace('Nan', np.nan)
-    return df
+    def show_df_info(self, df):
+        show_df_button = widgets.ToggleButton(description='Show 10 first rows')
+        show_nrows_ncols_button = widgets.ToggleButton(description='Show number of rows and columns')
+        show_cols_button = widgets.ToggleButton(description='Show columns')
+        show_info_button = widgets.ToggleButton(description='Show information')
+        show_cols_stats = widgets.ToggleButton(description='Show columns statistics')
+        ui = widgets.HBox(
+            [show_df_button, show_nrows_ncols_button, show_cols_button, show_info_button, show_cols_stats])
 
+        def buttons_actions(show_df_button=False, show_nrows_ncols_button=False, show_cols_button=False,
+                            show_info_button=False, show_cols_stats=False):
+            if show_df_button:
+                display(df.head(10))
+            if show_nrows_ncols_button:
+                print(f'Dataset has {df.shape[0]} rows and {df.shape[1]} columns')
+            if show_cols_button:
+                print('Columns of dataset:')
+                print(df.columns.values)
+            if show_info_button:
+                print(df.info())
+            if show_cols_stats:
+                display(df.describe())
 
-def format_columns(df, features_to_encode, features_to_drop, have_label=False, label=None, label_name=None):
-    df[features_to_encode] = df[features_to_encode].astype(str).applymap(str.lower).applymap(str.strip)
-    for i in features_to_encode:
-        df = pd.concat([df, pd.get_dummies(df[i], prefix=i)], axis=1)
-    df = df.loc[:, ~df.columns.str.endswith('nan')]
-    all_features = list(df.select_dtypes(['float', 'uint8']).columns)
-    features = list(set(all_features) - set(features_to_drop))
-    X = df[features]
-    X.replace([np.inf, -np.inf, np.nan], 0, inplace=True)
-    if have_label:
-        y = (df[label] == label_name) * 1
-        return X, y
-    return X
-
-
-def show_df_info(df):
-    show_df_button = widgets.ToggleButton(description='Show 10 first rows')
-    show_nrows_ncols_button = widgets.ToggleButton(description='Show number of rows and columns')
-    show_cols_button = widgets.ToggleButton(description='Show columns')
-    show_info_button = widgets.ToggleButton(description='Show information')
-    show_cols_stats = widgets.ToggleButton(description='Show columns statistics')
-    ui = widgets.HBox([show_df_button, show_nrows_ncols_button, show_cols_button, show_info_button, show_cols_stats])
-
-    def buttons_actions(show_df_button=False, show_nrows_ncols_button=False, show_cols_button=False, show_info_button=False, show_cols_stats=False):
-        if show_df_button:
-            display(df.head(10))
-        if show_nrows_ncols_button:
-            print(f'Dataset has {df.shape[0]} rows and {df.shape[1]} columns')
-        if show_cols_button:
-            print('Columns of dataset:')
-            print(df.columns.values)
-        if show_info_button:
-            print(df.info())
-        if show_cols_stats:
-            display(df.describe())
-
-    out = widgets.interactive_output(buttons_actions,
-                                     {'show_df_button': show_df_button,
-                                      'show_nrows_ncols_button': show_nrows_ncols_button,
-                                      'show_cols_button': show_cols_button,
-                                      'show_info_button': show_info_button,
-                                      'show_cols_stats': show_cols_stats})
-    display(ui, out)
-
-
-def get_model_prediction(model_path, X, thr, model_columns_path):
-    model = joblib.load(model_path)
-    with open(model_columns_path, 'rb') as handle:
-        model_columns = pickle.load(handle)
-    X = X[model_columns]
-    proba = model.predict_proba(X)[:, 1]
-    pred = proba > thr
-    return proba, pred
+        out = widgets.interactive_output(buttons_actions,
+                                         {'show_df_button': show_df_button,
+                                          'show_nrows_ncols_button': show_nrows_ncols_button,
+                                          'show_cols_button': show_cols_button,
+                                          'show_info_button': show_info_button,
+                                          'show_cols_stats': show_cols_stats})
+        display(ui, out)
 
 
-def get_model_decision(model_path, model_columns_path, X, thr):
-    proba, _ = get_model_prediction(model_path=model_path, model_columns_path=model_columns_path, X=X, thr=thr)
-    for i in range(X.shape[0]):
-        if proba[i] > thr:
-            decision = 'Yes'
-        else:
-            decision = 'No'
-        print('_'*100)
-        print()
-        print(f'Patient {i+1}')
-        print(f'Model decision for Covid-19: {decision} with Covid-19 probability {round(proba[i], 2)}')
-        print('_'*100)
+class ModelLauncher:
 
+    def get_model_prediction(self, model_path, X, thr, model_columns_path):
+        model = joblib.load(model_path)
+        with open(model_columns_path, 'rb') as handle:
+            model_columns = pickle.load(handle)
+        X = X[model_columns]
+        proba = model.predict_proba(X)[:, 1]
+        pred = proba > thr
+        return proba, pred
 
-def save_predictions_to_file(model_path, X, thr, model_columns_path, raw_df, new_file_name):
-    proba, pred = get_model_prediction(model_path=model_path, model_columns_path=model_columns_path, X=X, thr=thr)
-    predicted_decision = pd.Series(pred).replace({True: 'Yes', False: 'No'})
-    raw_df['Model_Probability'] = proba
-    raw_df['Model_Decision'] = predicted_decision
-    raw_df.to_csv(new_file_name)
-    print('Dataset saved!')
+    def get_model_decision(self, model_path, model_columns_path, X, thr):
+        proba, _ = self.get_model_prediction(model_path=model_path, model_columns_path=model_columns_path, X=X, thr=thr)
+        for i in range(X.shape[0]):
+            if proba[i] > thr:
+                decision = 'Yes'
+            else:
+                decision = 'No'
+            print('_' * 100)
+            print()
+            print(f'Patient {i + 1}')
+            print(f'Model decision for Covid-19: {decision} with Covid-19 probability {round(proba[i], 2)}')
+            print('_' * 100)
+
+    def save_predictions_to_file(self, model_path, X, thr, model_columns_path, raw_df, new_file_name):
+        proba, pred = self.get_model_prediction(model_path=model_path, model_columns_path=model_columns_path, X=X,
+                                                thr=thr)
+        predicted_decision = pd.Series(pred).replace({True: 'Yes', False: 'No'})
+        raw_df['Model_Probability'] = proba
+        raw_df['Model_Decision'] = predicted_decision
+        raw_df.to_csv(new_file_name)
+        print('Dataset saved!')
--- Diploma_Project/Analysis/plot_functions.py
+++ Diploma_Project/Analysis/plot_functions.py
@@ -8,191 +8,167 @@ from sklearn.neighbors import KernelDensity
 from scipy.signal import argrelextrema
 
 
-def plot_scatter(df):
-    numerical_columns = list(df.select_dtypes(['float']).columns)
-    categorical_columns = list(df.select_dtypes(['object']).columns)
-    x = widgets.Dropdown(
-        options=numerical_columns,
-        value=numerical_columns[0],
-        description='X:')
-    y = widgets.Dropdown(
-        options=numerical_columns,
-        value=numerical_columns[1],
-        description='Y:')
-    color = widgets.Dropdown(
-        options=categorical_columns,
-        value=categorical_columns[0],
-        description='Color:')
-    log_x = widgets.Checkbox(description='log_x', value=False)
-    log_y = widgets.Checkbox(description='log_y', value=False)
-    plot_button = widgets.ToggleButton(description='Plot Scatter')
-    ui = widgets.VBox([widgets.HBox([x, log_x, plot_button]), widgets.HBox([y, log_y]), widgets.HBox([color])])
+class Plotter:
 
-    def plot_features(x=numerical_columns, y=numerical_columns, color=categorical_columns, log_x=True, log_y=True, plot_button=False):
-        if plot_button:
-            fig = px.scatter(df, x=x, y=y, color=color, log_x=log_x, log_y=log_y, size_max=20)
-            fig.show()
+    def plot_scatter(self, df):
+        numerical_columns = list(df.select_dtypes(['float']).columns)
+        categorical_columns = list(df.select_dtypes(['object']).columns)
+        x = widgets.Dropdown(
+            options=numerical_columns,
+            value=numerical_columns[0],
+            description='X:')
+        y = widgets.Dropdown(
+            options=numerical_columns,
+            value=numerical_columns[1],
+            description='Y:')
+        color = widgets.Dropdown(
+            options=categorical_columns,
+            value=categorical_columns[0],
+            description='Color:')
+        log_x = widgets.Checkbox(description='log_x', value=False)
+        log_y = widgets.Checkbox(description='log_y', value=False)
+        plot_button = widgets.ToggleButton(description='Plot Scatter')
+        ui = widgets.VBox([widgets.HBox([x, log_x, plot_button]), widgets.HBox([y, log_y]), widgets.HBox([color])])
 
-    out = widgets.interactive_output(plot_features,
-                                     {'x': x, 'y': y, 'color': color, 'log_x': log_x, 'log_y': log_y, 'plot_button': plot_button})
-    display(ui, out)
+        def plot_features(x=numerical_columns, y=numerical_columns, color=categorical_columns, log_x=True, log_y=True,
+                          plot_button=False):
+            if plot_button:
+                fig = px.scatter(df, x=x, y=y, color=color, log_x=log_x, log_y=log_y, size_max=20)
+                fig.show()
 
+        out = widgets.interactive_output(plot_features,
+                                         {'x': x, 'y': y, 'color': color, 'log_x': log_x, 'log_y': log_y,
+                                          'plot_button': plot_button})
+        display(ui, out)
 
-def kdeplot_single(ax, df, is_0, feature, log_x, disease_name):
-    # prepare data for plotting
-    k_band_width = 25
-    kde_data = np.log10(df[df[feature] > 0][feature]) if log_x == True else df[df[feature] >= 0][feature]
-    data = kde_data.values
-    if len(data) > 0:
-        kde = KernelDensity(kernel='gaussian', bandwidth=(data.max() or 0.1) /
-                                                         k_band_width).fit(data.reshape(-1, 1))
-        _linspace = np.linspace(data.min(), data.max(), 100)
-        kde_v = np.exp(kde.score_samples(_linspace.reshape(-1, 1)))
-        gr_extr = argrelextrema(kde_v, np.greater)[0]
-        less_extr = argrelextrema(kde_v, np.less)[0]
-        # plotting
-        label = f'not-{disease_name}' if is_0 else disease_name
-        label = label + ': ' + str(len(data))
-        ax.plot(_linspace, kde_v, label=label)
-        ax.scatter(_linspace[gr_extr], kde_v[gr_extr], c='#aa0000')
-        ax.scatter(_linspace[less_extr], kde_v[less_extr], c='#00aa00')
-        ax.legend(fontsize=10)
-        ax.set_xlabel(feature)
-        ax.grid(True)
-    else:
-        print('No data')
+    def kdeplot_single(self, ax, df, is_0, feature, log_x, disease_name):
+        # prepare data for plotting
+        k_band_width = 25
+        kde_data = np.log10(df[df[feature] > 0][feature]) if log_x == True else df[df[feature] >= 0][feature]
+        data = kde_data.values
+        if len(data) > 0:
+            kde = KernelDensity(kernel='gaussian', bandwidth=(data.max() or 0.1) /
+                                                             k_band_width).fit(data.reshape(-1, 1))
+            _linspace = np.linspace(data.min(), data.max(), 100)
+            kde_v = np.exp(kde.score_samples(_linspace.reshape(-1, 1)))
+            gr_extr = argrelextrema(kde_v, np.greater)[0]
+            less_extr = argrelextrema(kde_v, np.less)[0]
+            # plotting
+            label = f'not-{disease_name}' if is_0 else disease_name
+            label = label + ': ' + str(len(data))
+            ax.plot(_linspace, kde_v, label=label)
+            ax.scatter(_linspace[gr_extr], kde_v[gr_extr], c='#aa0000')
+            ax.scatter(_linspace[less_extr], kde_v[less_extr], c='#00aa00')
+            ax.legend(fontsize=10)
+            ax.set_xlabel(feature)
+            ax.grid(True)
+        else:
+            print('No data')
 
+    def plot_distribution(self, df, column, disease_name):
+        df_1 = df[df[column] == disease_name]
+        df_0 = df[df[column] != disease_name]
+        numerical_columns = list(df.select_dtypes(['float']).columns)
+        x = widgets.Dropdown(
+            options=numerical_columns,
+            value=numerical_columns[0],
+            description='X:')
+        log_x = widgets.Checkbox(description='log_x', value=False)
+        plot_button = widgets.ToggleButton(description='Plot Distribution')
+        ui = widgets.HBox([x, log_x, plot_button])
 
-def plot_distribution(df, column, disease_name):
-    df_1 = df[df[column] == disease_name]
-    df_0 = df[df[column] != disease_name]
-    numerical_columns = list(df.select_dtypes(['float']).columns)
-    x = widgets.Dropdown(
-        options=numerical_columns,
-        value=numerical_columns[0],
-        description='X:')
-    log_x = widgets.Checkbox(description='log_x', value=False)
-    plot_button = widgets.ToggleButton(description='Plot Distribution')
-    ui = widgets.HBox([x, log_x, plot_button])
+        def plot_features(x=numerical_columns, log_x=True, plot_button=False):
+            if plot_button:
+                fig, ax = plt.subplots(ncols=1, nrows=1, figsize=(9, 4))
+                self.kdeplot_single(ax, df_1, is_0=0, feature=x, log_x=log_x, disease_name=disease_name)
+                self.kdeplot_single(ax, df_0, is_0=1, feature=x, log_x=log_x, disease_name=disease_name)
+                ax.set_title(f'Distribution of {x}')
+                fig.show()
 
-    def plot_features(x=numerical_columns, log_x=True, plot_button=False):
-        if plot_button:
-            fig, ax = plt.subplots(ncols=1, nrows=1, figsize=(9, 4))
-            kdeplot_single(ax, df_1, is_0=0, feature=x, log_x=log_x, disease_name=disease_name)
-            kdeplot_single(ax, df_0, is_0=1, feature=x, log_x=log_x, disease_name=disease_name)
-            ax.set_title(f'Distribution of {x}')
-            fig.show()
+        out = widgets.interactive_output(plot_features,
+                                         {'x': x, 'log_x': log_x, 'plot_button': plot_button})
+        display(ui, out)
 
-    out = widgets.interactive_output(plot_features,
-                                     {'x': x, 'log_x': log_x, 'plot_button': plot_button})
-    display(ui, out)
+    def plot_bar_chart(self, df):
+        categorical_columns = list(df.select_dtypes(['object']).columns)
+        x = widgets.Dropdown(
+            options=categorical_columns,
+            value=categorical_columns[0],
+            description='X:')
+        splitting = widgets.Dropdown(
+            options=categorical_columns,
+            value=categorical_columns[1],
+            description='Splitting:')
+        plot_button = widgets.ToggleButton(description='Plot Bar')
+        ui = widgets.VBox([widgets.HBox([x, plot_button]), widgets.HBox([splitting])])
 
+        def plot_features(x=categorical_columns, splitting=categorical_columns, plot_button=False):
+            if plot_button:
+                sns.countplot(x=x, hue=splitting, data=df)
 
-def plot_bar_chart(df):
-    categorical_columns = list(df.select_dtypes(['object']).columns)
-    x = widgets.Dropdown(
-        options=categorical_columns,
-        value=categorical_columns[0],
-        description='X:')
-    splitting = widgets.Dropdown(
-        options=categorical_columns,
-        value=categorical_columns[1],
-        description='Splitting:')
-    plot_button = widgets.ToggleButton(description='Plot Bar')
-    ui = widgets.VBox([widgets.HBox([x, plot_button]), widgets.HBox([splitting])])
+        out = widgets.interactive_output(plot_features,
+                                         {'x': x, 'splitting': splitting, 'plot_button': plot_button})
+        display(ui, out)
 
-    def plot_features(x=categorical_columns, splitting=categorical_columns, plot_button=False):
-        if plot_button:
-            sns.countplot(x=x, hue=splitting, data=df)
+    def plot_distplot(self, df):
+        numerical_columns = list(df.select_dtypes(['float']).columns)
+        x = widgets.Dropdown(
+            options=numerical_columns,
+            value=numerical_columns[0],
+            description='X:')
+        plot_button = widgets.ToggleButton(description='Plot Distplot')
+        ui = widgets.VBox([widgets.HBox([x, plot_button])])
 
-    out = widgets.interactive_output(plot_features,
-                                     {'x': x, 'splitting': splitting, 'plot_button': plot_button})
-    display(ui, out)
+        def plot_features(x=numerical_columns, plot_button=False):
+            if plot_button:
+                sns.distplot(df[x])
 
+        out = widgets.interactive_output(plot_features,
+                                         {'x': x, 'plot_button': plot_button})
+        display(ui, out)
 
-def plot_distplot(df):
-    numerical_columns = list(df.select_dtypes(['float']).columns)
-    x = widgets.Dropdown(
-        options=numerical_columns,
-        value=numerical_columns[0],
-        description='X:')
-    plot_button = widgets.ToggleButton(description='Plot Distplot')
-    ui = widgets.VBox([widgets.HBox([x, plot_button])])
+    def plot_boxplot(self, df):
+        numerical_columns = list(df.select_dtypes(['float']).columns)
+        x = widgets.Dropdown(
+            options=numerical_columns,
+            value=numerical_columns[0],
+            description='X:')
+        plot_button = widgets.ToggleButton(description='Plot Boxplot')
+        ui = widgets.VBox([widgets.HBox([x, plot_button])])
 
-    def plot_features(x=numerical_columns, plot_button=False):
-        if plot_button:
-            sns.distplot(df[x])
+        def plot_features(x=numerical_columns, plot_button=False):
+            if plot_button:
+                _, axes = plt.subplots(1, 2, sharey=True, figsize=(6, 4))
+                sns.boxplot(data=df[x], ax=axes[0])
+                sns.violinplot(data=df[x], ax=axes[1])
 
-    out = widgets.interactive_output(plot_features,
-                                     {'x': x, 'plot_button': plot_button})
-    display(ui, out)
+        out = widgets.interactive_output(plot_features,
+                                         {'x': x, 'plot_button': plot_button})
+        display(ui, out)
 
+    def plot_corr(self, df):
+        numerical_columns = list(df.select_dtypes(['float']).columns)
+        plot_button = widgets.ToggleButton(description='Plot Correlation')
+        ui = widgets.VBox([widgets.HBox([plot_button])])
 
-def plot_boxplot(df):
-    numerical_columns = list(df.select_dtypes(['float']).columns)
-    x = widgets.Dropdown(
-        options=numerical_columns,
-        value=numerical_columns[0],
-        description='X:')
-    plot_button = widgets.ToggleButton(description='Plot Boxplot')
-    ui = widgets.VBox([widgets.HBox([x, plot_button])])
+        def plot_features(plot_button=False):
+            if plot_button:
+                corr_matrix = df[numerical_columns].corr()
+                sns.heatmap(corr_matrix)
 
-    def plot_features(x=numerical_columns, plot_button=False):
-        if plot_button:
-            _, axes = plt.subplots(1, 2, sharey=True, figsize=(6, 4))
-            sns.boxplot(data=df[x], ax=axes[0])
-            sns.violinplot(data=df[x], ax=axes[1])
+        out = widgets.interactive_output(plot_features,
+                                         {'plot_button': plot_button})
+        display(ui, out)
 
-    out = widgets.interactive_output(plot_features,
-                                     {'x': x, 'plot_button': plot_button})
-    display(ui, out)
+    def plot_hist(self, df):
+        numerical_columns = list(df.select_dtypes(['float']).columns)
+        plot_button = widgets.ToggleButton(description='Plot Histograms')
+        ui = widgets.VBox([widgets.HBox([plot_button])])
 
+        def plot_features(plot_button=False):
+            if plot_button:
+                df[numerical_columns].hist(figsize=(20, 20))
 
-def plot_corr(df):
-    numerical_columns = list(df.select_dtypes(['float']).columns)
-    plot_button = widgets.ToggleButton(description='Plot Correlation')
-    ui = widgets.VBox([widgets.HBox([plot_button])])
-
-    def plot_features(plot_button=False):
-        if plot_button:
-            corr_matrix = df[numerical_columns].corr()
-            sns.heatmap(corr_matrix)
-
-    out = widgets.interactive_output(plot_features,
-                                     {'plot_button': plot_button})
-    display(ui, out)
-
-
-def plot_hist(df):
-    numerical_columns = list(df.select_dtypes(['float']).columns)
-    plot_button = widgets.ToggleButton(description='Plot Histograms')
-    ui = widgets.VBox([widgets.HBox([plot_button])])
-
-    def plot_features(plot_button=False):
-        if plot_button:
-            df[numerical_columns].hist(figsize=(20, 20))
-
-    out = widgets.interactive_output(plot_features,
-                                     {'plot_button': plot_button})
-    display(ui, out)
-
-
-def plot_map(df):
-    categorical_columns = list(df.select_dtypes(['object']).columns)
-    x = widgets.Dropdown(
-        options=categorical_columns,
-        value=categorical_columns[0],
-        description='Geo column:')
-    plot_button = widgets.ToggleButton(description='Plot Map')
-    ui = widgets.VBox([widgets.HBox([x, plot_button])])
-
-    def plot_features(x=categorical_columns, plot_button=False):
-        if plot_button:
-            fig = px.scatter_geo(df, locations="iso_alpha", color="continent",
-                                 hover_name="country", size="pop",
-                                 projection="natural earth")
-            fig.show()
-
-    out = widgets.interactive_output(plot_features,
-                                     {'x': x, 'plot_button': plot_button})
-    display(ui, out)
+        out = widgets.interactive_output(plot_features,
+                                         {'plot_button': plot_button})
+        display(ui, out)
